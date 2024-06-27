1acea14483cd98077ed7e7cef91c9e8feb442472
Vukkisila-Shobhitha <shobitha6294@gmail.com>
2024-06-27 19:15:01 +0530

    Updated the CORS error

--- index.js
+++ index.js
@@ -21,7 +21,19 @@ const app = express();
 app.use(express.json());
 
 // CORS
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://magenta-alpaca-432d58.netlify.app'
+];
+app.use(cors({ origin: function(origin, callback) {
+  // Allow requests with no origin (like mobile apps or curl requests)
+  if (!origin) return callback(null, true);
+  if (allowedOrigins.indexOf(origin) === -1) {
+    const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+    return callback(new Error(msg), false);
+  }
+  return callback(null, true);
+}, credentials: true }));
 
 // Use express router
 app.use('/', require("./Routes"));
